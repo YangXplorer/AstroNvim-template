7bb3c2a1bb0d3cba0db26671b4684d6918eee884
YangXplorer <yangxianglong@bridge.vc>
2024-04-15 14:49:33 +0900

    first commit

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,19 @@
+return {
+  -- You can also add new plugins here as well:
+  -- Add plugins, the lazy syntax
+  -- "andweeb/presence.nvim",
+  -- {
+  --   "ray-x/lsp_signature.nvim",
+  --   event = "BufRead",
+  --   config = function()
+  --     require("lsp_signature").setup()
+  --   end,
+  -- },
+  {
+    "github/copilot.vim",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function() vim.g.copilot_no_tab_map = true end,
+  },
+}
+
