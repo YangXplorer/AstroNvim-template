86a195847fbcb855e658d36a293ae147dcfafbad
YangXplorer <yangxianglong@bridge.vc>
2024-04-18 13:39:50 +0900

    dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -26,9 +26,9 @@ return {
       opts.button("m", "󱌣   Mason", ":Mason<CR>"),
       opts.button("u", "󰂖   Update plugins", "<cmd>lua require('lazy').sync()<CR>"),
       opts.button("d", "   datahub", ":cd ~/Projects/Python/datahub-backend/ | :edit .<CR>"),
-      opts.button("h", "   neohub", ":cd ~/Projects/JavaScript/neohub/ | :edit .<CR>"),
-      opts.button("b", "   backend", ":cd ~/Projects/Golang/bridge-systems-backend/ | :edit .<CR>"),
-      opts.button("i", "   i-backend", ":cd ~/Projects/Golang/medical-eye-backend/ | :edit .<CR>"),
+      opts.button("h", "   neohub", ":cd ~/Projects/NeoHub/NeoHub/ | :edit .<CR>"),
+      opts.button("b", "   backend", ":cd ~/Projects/NeoHub/NeoHub-Backend/ | :edit .<CR>"),
+      opts.button("l", "   learning", ":cd ~/Projects/Learning/ | :edit .<CR>"),
       opts.button("v", "   astronvim", ":cd ~/.config/nvim/ | :edit .<CR>"),
     }
   end,
